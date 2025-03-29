e3bc56286de962128455c68d88ee77497bd38f37
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2025-03-29 15:12:48 +0530

    feat(docker): Add Docker support and update MongoDB config
    - Updated MongoDB connection string placeholders in
      `DependencyInjection.cs` and `appsettings.json` for
      consistency.
    
    - Added `<DockerComposeProjectPath>` to
      `OrdersMicroservice.API.csproj` for Docker integration.
    
    - Introduced `docker-compose.dcproj` to define Docker
      project settings.
    
    - Created `docker-compose.override.yml` for local
      development environment variables and port mappings.
    
    - Added `docker-compose.yml` to specify Docker image
      and build context for `ordersmicroservice.api`.
    
    - Modified `eCommerceSolution.OrdersService.sln` to
      include the new Docker Compose project.
    
    - Updated `launchSettings.json` to add a Docker
      Compose profile for debugging.

--- DataAccessLayer/DependencyInjection.cs
+++ DataAccessLayer/DependencyInjection.cs
@@ -13,8 +13,8 @@ namespace DataAccessLayer
         {
             string connectionStringTemplate =
                 configuration.GetConnectionString("MongoDB")!;
-           string connectionString = connectionStringTemplate.Replace("$MONGO_HOST", Environment.GetEnvironmentVariable("MONGODB_HOST"))
-                .Replace("$MONGO_PORT", Environment.GetEnvironmentVariable("MONGODB_PORT"));
+           string connectionString = connectionStringTemplate.Replace("$MONGODB_HOST", Environment.GetEnvironmentVariable("MONGODB_HOST"))
+                .Replace("$MONGODB_PORT", Environment.GetEnvironmentVariable("MONGODB_PORT"));
 
             services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
 
